368449fdb98e9481cd58d6097e05d825f7463047
Taimur Mushtaq <taimur.1989@gmail.com>
2021-05-04 00:52:15 +0500

    - Adding Pods Directory and Podfile.lock file in ignore - Added public and private keys

--- The Hitchhiker Prophecy/Networking/Base/NetworkConstants.swift
+++ The Hitchhiker Prophecy/Networking/Base/NetworkConstants.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum NetworkConstants {
     static let baseUrl = "https://gateway.marvel.com"
-    static let publicKey = "" // TODO: Add your marvel keys
-    static let privateKey = "" // TODO: Add your marvel keys
+    
+    // I obtained keys by following the link mentioned in the document.
+    // I did complete the registration process at Marvel
+    static let publicKey = "4da3a810f9b7c8715aaf3756db3586f4"
+    static let privateKey = "afff8531b8b3925281471041b1a4ad64a47e6504"
 }
