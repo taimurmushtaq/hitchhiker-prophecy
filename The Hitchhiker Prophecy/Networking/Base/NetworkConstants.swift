c02a34050ba82c9e416baa8f77d902a1be0eb3b1
Taimur Mushtaq <taimur.1989@gmail.com>
2021-05-04 01:18:53 +0500

    - Using Apple's CryptoKit to create hash value to pass in API

--- The Hitchhiker Prophecy/Networking/Base/NetworkConstants.swift
+++ The Hitchhiker Prophecy/Networking/Base/NetworkConstants.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CryptoKit
 
 enum NetworkConstants {
     static let baseUrl = "https://gateway.marvel.com"
@@ -15,4 +16,13 @@ enum NetworkConstants {
     // I did complete the registration process at Marvel
     static let publicKey = "4da3a810f9b7c8715aaf3756db3586f4"
     static let privateKey = "afff8531b8b3925281471041b1a4ad64a47e6504"
+}
+
+extension NetworkConstants {
+    static func getHashValue(ts: String) -> String {
+        let stringValue = ts + NetworkConstants.publicKey + NetworkConstants.privateKey
+        
+        let digest = Insecure.MD5.hash(data: stringValue.data(using: .utf8) ?? Data())
+        return digest.map { String(format: "%02hhx", $0) }.joined()
+    }
 }
