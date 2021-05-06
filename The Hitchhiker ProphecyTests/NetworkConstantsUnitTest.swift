1097ee1f81dceb9a43b721f406abc6d0a7c82658
Taimur Mushtaq <taimur.1989@gmail.com>
2021-05-06 21:59:56 +0500

    - Remove empty methods

--- The Hitchhiker ProphecyTests/NetworkConstantsUnitTest.swift
+++ The Hitchhiker ProphecyTests/NetworkConstantsUnitTest.swift
@@ -9,17 +9,6 @@
 import XCTest
 
 class NetworkConstantsUnitTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-}
-
-extension NetworkConstantsUnitTest {
     func testMD5HashValue() {
         XCTAssertEqual(NetworkConstants.getHashValue(ts: ""), "384bec93b7c1a61aa4e4b7366d7b4adf")
         XCTAssertEqual(NetworkConstants.getHashValue(ts: "1"), "6d9ff321d28b685aaa060d8d7131383b")
